df2c395e8ff129ec48eb1dc9c416a0fa6b1597f7
ThameeraFernando <sithnuwan111@gmail.com>
2022-05-01 15:35:21 +0530

    create a token and send

--- controllers/main.js
+++ controllers/main.js
@@ -1,5 +1,20 @@
+const CustomerAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
 const login = async (req, res) => {
-  res.send("Fake Login/Register/SignUp");
+  const { username, password } = req.body;
+  // console.log(username, password);
+  //check the user name and password
+  if (!username || !password) {
+    throw new CustomerAPIError("Please provide email and password.", 400);
+  }
+  //sample id
+  const id = new Date().getDate();
+
+  const token = jwt.sign({ username, id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  res.status(200).json({ msg: "user Created.", token });
+  // res.send("Fake Login/Register/SignUp");
 };
 
 const dashboard = async (req, res) => {
